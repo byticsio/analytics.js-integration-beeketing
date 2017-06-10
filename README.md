917bed7103a5fe04b47e7fc7ec0646d80ebcccd4
François de Metz <francois@2metz.fr>
2017-06-10 15:48:32 +0200

    Update README and files.

---
+++ README.md
@@ -0,0 +1,11 @@
+# analytics.js-integration-beeketing [![Build Status][ci-badge]][ci-link]
+
+Beeketing integration for [Analytics.js][].
+
+## License
+
+Released under the [MIT license](LICENSE).
+
+[Analytics.js]: https://segment.com/docs/libraries/analytics.js/
+[ci-link]: https://circleci.com/gh/byticsio/analytics.js-integration-beeketing
+[ci-badge]: https://circleci.com/gh/byticsio/analytics.js-integration-beeketing.svg?style=svg
---
+++ test/index.test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Analytics = require('@segment/analytics.js-core').constructor;
+var integration = require('@segment/analytics.js-integration');
+var sandbox = require('@segment/clear-env');
+var tester = require('@segment/analytics.js-integration-tester');
+var Beeketing = require('../lib/');
+
+describe('Beeketing', function() {
+  var analytics;
+  var beeketing;
+  var options = {
+    'token': 'test'
+  };
+
+  beforeEach(function() {
+    analytics = new Analytics();
+    beeketing = new Beeketing(options);
+    analytics.use(Beeketing);
+    analytics.use(tester);
+    analytics.add(beeketing);
+  });
+
+  afterEach(function() {
+    analytics.restore();
+    analytics.reset();
+    beeketing.reset();
+    sandbox();
+  });
+
+  it('should have the right settings', function() {
+    analytics.compare(Beeketing, integration('Beeketing')
+      .global('BeeketingTrack')
+      .option('token', ''));
+  });
+});
